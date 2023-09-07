f7dde9051a61852e9d001f602cccc5dd51d0ea08
zobayerdev <zobayer.dev@gmail.com>
2023-09-08 00:15:15 +0600

    project create and project procedure is completely defined

---
+++ app/src/main/java/com/trodev/mycoachingstudents/MainActivity.java
@@ -0,0 +1,224 @@
+package com.trodev.mycoachingstudents;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    SmoothBottomBar smoothBottomBar;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private long pressedTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*init all drawer layout*/
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        /*init views*/
+        smoothBottomBar = findViewById(R.id.bottombar);
+
+
+        // #######################
+        // Drawer Layout implement
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // #################################################################
+        // navigation view work process
+        navigationView.setNavigationItemSelectedListener(this::onOptionsItemSelected);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        /*When apps start show HomeFragments*/
+        setTitle("Dashboard");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, new HomeFragment());
+        fragmentTransaction.commit();
+
+
+        /*set all status bar, navigation bar, toolbar color*/
+        smoothBottomBar.setBarBackgroundColor(Color.parseColor("#2196F3"));
+        getWindow().setNavigationBarColor(Color.parseColor("#2196F3"));
+        getWindow().setStatusBarColor(Color.parseColor("#2196F3"));
+
+
+        /*smooth bar working process*/
+        smoothBottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+
+                if (i == 0) {
+                    setTitle("Dashboard");
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayout, new HomeFragment());
+                    fragmentTransaction.commit();
+
+                    /*set all status bar, navigation bar, toolbar color*/
+                    smoothBottomBar.setBarBackgroundColor(Color.parseColor("#2196F3"));
+                    getWindow().setNavigationBarColor(Color.parseColor("#2196F3"));
+                    getWindow().setStatusBarColor(Color.parseColor("#2196F3"));
+
+                }
+
+                if (i == 1) {
+                    setTitle("Lecture Sheets");
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayout, new LectureFragment());
+                    fragmentTransaction.commit();
+
+                    /*set all status bar, navigation bar, toolbar color*/
+                    smoothBottomBar.setBarBackgroundColor(Color.parseColor("#2196F3"));
+                    getWindow().setNavigationBarColor(Color.parseColor("#2196F3"));
+                    getWindow().setStatusBarColor(Color.parseColor("#2196F3"));
+                }
+
+                if (i == 2) {
+                    setTitle("About us");
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayout, new AboutusFragment());
+                    fragmentTransaction.commit();
+
+                    /*set all status bar, navigation bar, toolbar color*/
+                    smoothBottomBar.setBarBackgroundColor(Color.parseColor("#2196F3"));
+                    getWindow().setNavigationBarColor(Color.parseColor("#2196F3"));
+                    getWindow().setStatusBarColor(Color.parseColor("#2196F3"));
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.menu_notification) {
+            Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
+
+        } else if (itemId == R.id.menu_privacy) {
+            Toast.makeText(this, "Privacy Policy", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, PrivacyPolicy.class));
+
+        } else if (itemId == R.id.menu_share) {
+            Toast.makeText(this, "Share our apps", Toast.LENGTH_SHORT).show();
+            try {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My Coaching Students");
+                String shareMessage = "\nMy Coaching Students App Download now\n\n";
+                shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName();
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                startActivity(Intent.createChooser(shareIntent, "choose one"));
+
+            } catch (Exception e) {
+                Toast.makeText(this, "Unable to share!", Toast.LENGTH_SHORT).show();
+            }
+        } else if (itemId == R.id.menu_rating) {
+            Toast.makeText(this, "Rate Us", Toast.LENGTH_SHORT).show();
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+                Toast.makeText(this, "Rate us", Toast.LENGTH_SHORT).show();
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+            }
+
+        } else if (itemId == R.id.menu_apps) {
+            Toast.makeText(this, "Our Apps", Toast.LENGTH_SHORT).show();
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=6580660399707616800")));
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=6580660399707616800")));
+            }
+
+        } else if (itemId == R.id.menu_developer) {
+            Toast.makeText(this, "Developer Contact", Toast.LENGTH_SHORT).show();
+            final Dialog dialog = new Dialog(this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.contact_bottomsheet_layout);
+
+            dialog.show();
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        } else if (itemId == R.id.menu_company) {
+            Toast.makeText(this, "Company Website", Toast.LENGTH_SHORT).show();
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.trodev.com")));
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.trodev.com")));
+            }
+
+        } else if (itemId == R.id.menu_email) {
+            Toast.makeText(this, "Send Us Mail", Toast.LENGTH_SHORT).show();
+            try {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"help.trodev@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Help of");
+                intent.putExtra(Intent.EXTRA_TEXT, "Assalamualaikum, ");
+                startActivity(Intent.createChooser(intent, ""));
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(this, "Unable to access email", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // In this code, android lifecycle exit on 2 times back-pressed.
+    @Override
+    public void onBackPressed() {
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        pressedTime = System.currentTimeMillis();
+    }
+}
---
+++ app/src/main/java/com/trodev/mycoachingstudents/PrivacyPolicy.java
@@ -0,0 +1,18 @@
+package com.trodev.mycoachingstudents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+
+        getSupportActionBar().setTitle("Privacy policy");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
